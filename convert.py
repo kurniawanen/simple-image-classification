bb5cf531f32d93feec6b95c9e8c70df0977735da
Lantang Satriatama <Lantang Satriatama>
2017-05-13 20:10:13 +0700

    add code comments

--- convert.py
+++ convert.py
@@ -6,30 +6,38 @@ import os
 
 def convert_to_ndarray(image,size):
 	#image: string, size: tuple[x,y]
+	#read as greyscale and then convert uint16 to float
 	arr 		= img_as_float(io.imread(image,as_grey = True));
+	#self explain
 	avg 		= arr.mean();
 	mx 			= arr.max();
 	mn 			= arr.min();
 	threshold 	= ((3*avg) + mx) / 4;
+	#if value>threshold 1 else 0, python magic :v
 	arr			= (arr > threshold).astype(float);
-	arr 		= resize(arr,[56,56]);
+	#resize to 56x56
+	arr 		= resize(arr,size);
 	arr 		= arr.astype(float);
+	#normalize to 0 - 1
 	arr 		*= 1.0/arr.max();
+	#flatten to 1 dimensional array[3136]
+	arr 		= arr.flatten(); 
 	return arr;
 
 def train_all_image_in_folder(foldername,status):
-	#foldername: string, status: one hot [0,1,0,0]
+	#foldername: string, status: one hot [0,1] (yes) or [1,0] (no)
 	trlabel = [];
 	trfeat = [];
+	#input all image file in folder
 	for file in os.listdir(foldername):
 		if file.endswith('.png') or file.endswith('.jpg') or file.endswith('.jpeg'):
 			train_array = convert_to_ndarray(foldername + '/' + file,[56,56]);
-			trfeat.append(train_array.flatten());
+			trfeat.append(train_array);
 			trlabel.append(status);
 	train(trfeat, trlabel);
 	
 def predict_image(image):
-	test = [];
-	train_array = convert_to_ndarray(image,[56,56]);
-	test.append(train_array.flatten());
+	test 	= [];
+	array 	= convert_to_ndarray(image,[56,56]);
+	test.append(array);
 	predict(test);
--- learning.py
+++ learning.py
@@ -12,7 +12,8 @@ def first_train(trfeat,trlabel):
 	import pandas as pd
 	import numpy as np
 	import datetime
-	#using tensorflow simple neural network for classify (overkill, lol)
+	#using tensorflow simple neural network (1 [3136] weight layer + 1 [2] bias) for classify (overkill, lol)
+	#read this for reference:https://www.tensorflow.org/get_started/mnist/beginners
 	import tensorflow as tf
 	tf.reset_default_graph()
 
@@ -25,32 +26,28 @@ def first_train(trfeat,trlabel):
 	y_ = tf.placeholder(tf.float32, [None, 2])
 	W = tf.Variable(tf.zeros([3136, 2]))
 	b = tf.Variable(tf.zeros([2]))
-
 	y = tf.matmul(x, W) + b
 	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_))
-
 	train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
-
-	sess = tf.Session() #is this can be changed to tf.Session()?
+	
+	sess = tf.Session()
 	tf.global_variables_initializer().run(session = sess)
 
 	#training process
 	for epoch in range(training_epochs):
-		#print (epoch);
 		for i in range(total_batch-1):
 			batch_x = trfeat[i*batch_size:(i+1)*batch_size]
 			batch_y = trlabel[i*batch_size:(i+1)*batch_size]
 			sess.run(train_step, feed_dict={x: batch_x, y_: batch_y})
+	
 	#saving weight and bias
-
-	print (np.count_nonzero(~np.isnan(W.eval(session = sess))))
-	print (np.count_nonzero(~np.isnan(b.eval(session = sess))))
 	saver = tf.train.Saver([W, b])
 	saver.save(sess, './model.ckpt')
 	print ('model saved!');
+	
 	#logging
 	log = open("log.txt","a+");
-	log.write(str(trlabel) + ' ' + 'Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()));
+	log.write("Train:" + ' ' + 'Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()));
 	log.close();
 
 def next_train(trfeat,trlabel):
@@ -58,7 +55,8 @@ def next_train(trfeat,trlabel):
 	import pandas as pd
 	import numpy as np
 	import datetime
-	#using tensorflow simple neural network for classify (overkill, lol)
+	#using tensorflow simple neural network (1 [3136] weight layer + 1 [2] bias) for classify (overkill, lol)
+	#read this for reference:https://www.tensorflow.org/get_started/mnist/beginners
 	import tensorflow as tf
 	tf.reset_default_graph()
 
@@ -71,65 +69,54 @@ def next_train(trfeat,trlabel):
 	y_ = tf.placeholder(tf.float32, [None, 2])
 	W = tf.Variable(tf.zeros([3136, 2]))
 	b = tf.Variable(tf.zeros([2]))
-
 	y = tf.matmul(x, W) + b
 	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_))
-
 	train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
 
-	sess = tf.Session() #is this can be changed to tf.Session()?
+	sess = tf.Session()
 	tf.global_variables_initializer().run(session = sess)
 
+	#loading model (weight and bias from previous learn)
 	saver = tf.train.Saver([W, b])
 	saver.restore(sess, './model.ckpt')
 	print ('model restored!')
+	
 	#training process
 	for epoch in range(training_epochs):
-		#print (epoch);
 		for i in range(total_batch-1):
 			batch_x = trfeat[i*batch_size:(i+1)*batch_size]
 			batch_y = trlabel[i*batch_size:(i+1)*batch_size]
 			sess.run(train_step, feed_dict={x: batch_x, y_: batch_y})
+	
 	#saving weight and bias
-
-	print (np.count_nonzero(~np.isnan(W.eval(session = sess))))
-	print (np.count_nonzero(~np.isnan(b.eval(session = sess))))
 	saver = tf.train.Saver([W, b])
 	saver.save(sess, './model.ckpt')
 	print ('model saved!');
+	
 	#logging
 	log = open("log.txt","a+");
-	log.write(str(trlabel) + ' ' + 'Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()));
+	log.write("Train:" + ' ' + 'Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()));
 	log.close();
 
 def predict(trfeat):
 	import pandas as pd
 	import numpy as np
 	import datetime
-	#using tensorflow simple neural network for classify (overkill, lol)
 	import tensorflow as tf
 	tf.reset_default_graph()
 
-	learning_rate = 0.5
-
 	x = tf.placeholder(tf.float32, [None,3136])
-	#y_ = tf.placeholder(tf.float32, [None, 2])
 	W = tf.Variable(tf.zeros([3136, 2]))
 	b = tf.Variable(tf.zeros([2]))
-
 	y = tf.matmul(x, W) + b
-	#cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_))
 
-	#train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
-
-	sess = tf.Session() #is this can be changed to tf.Session()?
+	sess = tf.Session()
 	tf.global_variables_initializer().run(session = sess)
 
+	#loading the model
 	saver = tf.train.Saver([W, b])
 	saver.restore(sess, './model.ckpt')
 	print ('model restored!')
-	#predict
-	#print (trfeat)
-	#print(W.eval(session = sess))
-	#print (b.eval(session = sess))
+	
+	#predict (1 for yes and 0 for no)
 	print(np.argmax(sess.run(y, feed_dict={x: trfeat})))
